0e7d9576700f90d0614487fa731a0b102557f302
Kiss, Tibor <tibor.kiss01@sap.com>
2023-09-14 16:55:53 +0200

    Azure 7 hetes képzés 2023.

--- src/PostApi.js
+++ src/PostApi.js
@@ -90,7 +90,7 @@ class PostApi extends React.Component {
         const appWidth = window.innerWidth
         return (
             <div>
-                <p class="pageSubHeader">(Azure 6 hetes képzés 2023.05.22.)</p>
+                <p class="pageSubHeader">(Azure 7 hetes képzés 2023.)</p>
                 <div class="row">
                     <div class="col-md-6 offset-md-3">
                         <div class="form-container" style={appWidth < 500 ? { paddingLeft: "50px" } : { paddingLeft: "35%" }}>
